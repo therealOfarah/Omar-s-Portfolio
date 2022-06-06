aa49837b8767fcb6fca7fbb9fee9e425c45b8325
therealofarah <omaarfaarah2019@gmail.com>
2022-06-06 19:32:10 -0400

    add a data js file and start export

---
+++ js/data.js
@@ -0,0 +1,9 @@
+const projects = {
+    name : "Tic-Tac-Toe",
+    bio : "The above project is a game of Tic-Tac-Toe",
+    weblink : "https://therealofarah-ttt.netlify.app",
+}
+
+export{
+  projects
+}
