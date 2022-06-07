4528f60f34cb5d800f2d06ab69f8e34e2568926d
therealofarah <omaarfaarah2019@gmail.com>
2022-06-07 18:12:35 -0400

    add bootstrap

--- js/data.js
+++ js/data.js
@@ -1,9 +1,14 @@
 const projects = {
     name : "Tic-Tac-Toe",
     bio : "The above project is a game of Tic-Tac-Toe",
+    image : 
     weblink : "https://therealofarah-ttt.netlify.app",
 }
 
+const resume = {
+
+}
 export{
   projects
 }
+  
