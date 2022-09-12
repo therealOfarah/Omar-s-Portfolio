40f620830d12174907d843d8a67d081b6551a9ed
Omar Farah <omaarfaarah2019@gmail.com>
2022-09-12 16:29:44 -0400

    add portfolio project

--- js/data.js
+++ js/data.js
@@ -1,24 +1,31 @@
 const projects = [
   {
+    name: "Portfolio",
+    image: "/images/portfolio.png",
+    weblink: "https://omarfarahportfolio.netlify.app/index.html",
+    github: "https://github.com/therealOfarah/Omar-s-Portfolio",
+    bio: "This app showcases all of my work and links to deployments for each project that I have done. It was built using HTML,CSS and Javascript",
+  },
+  {
     name: "Tic-Tac-Toe",
     image: "/images/Tic-Tac-Toe.png",
     weblink: "https://therealofarah-ttt.netlify.app",
     github: "https://github.com/therealOfarah/Tic-Tac-Toe",
-    bio: "An app built using html,css and javascript",
+    bio: "I created a web game that lets you play Tic-Tac-Toe. It was built using HTML,CSS and Javascript",
   },
   {
     name: "Ani-Review",
     image: "/images/Screenshot.png",
     weblink: "https://omars-ani-review1.herokuapp.com/",
     github: "https://github.com/therealOfarah/Ani-Review",
-    bio: "An app built using the MEN Stack and Heroku",
+    bio: "A web app that allows you to search up anime and helps you find what you are looking for. This app was built using MEN stack(MongoDb, Express ,Node.js), Anime API, and EJS",
   },
   {
     name: "Fitness Bro",
     image: "/images/fitness-bro.png",
     weblink: "https://fitness-bro.netlify.app/",
     github: "https://github.com/therealOfarah/fitness-bro-front-end",
-    bio: "An app built using the MERN Stack and Heroku",
+    bio: "My group and I built an amazing app that helps you keep track of each workout you have done and also helps you keep track of your daily food intake. This app was built using MERN stack(MongoDb, Express ,React.js ,Node.js), Workout API and Nutrition API.",
   },
 ]
 
