1dc6f9eaf2e731c7b1ae479db125a99bb94678f6
Omar Farah <omaarfaarah2019@gmail.com>
2023-01-25 13:02:14 -0500

    update navbar and projects page

--- js/data.js
+++ js/data.js
@@ -1,25 +1,25 @@
 const projects = [
-  {
-    name: "Portfolio",
-    image: "/images/portfolio.png",
-    weblink: "https://omarfarahportfolio.netlify.app/index.html",
-    github: "https://github.com/therealOfarah/Omar-s-Portfolio",
-    bio: "This app showcases all of my work and links to deployments for each project that I have done so far. It was built using HTML,CSS and Javascript.",
-  },
-  {
-    name: "Tic-Tac-Toe",
-    image: "/images/Tic-Tac-Toe.png",
-    weblink: "https://therealofarah-ttt.netlify.app",
-    github: "https://github.com/therealOfarah/Tic-Tac-Toe",
-    bio: "I created a web game that lets you play Tic-Tac-Toe. It was built using HTML,CSS and Javascript.",
-  },
-  {
-    name: "Ani-Review",
-    image: "/images/Screenshot.png",
-    weblink: "https://omarsanireview1.fly.dev/",
-    github: "https://github.com/therealOfarah/Ani-Review",
-    bio: "A web app that allows you to search up anime and helps you find what you are looking for. This app was built using MEN stack(MongoDb, Express ,Node.js), Anime API, and EJS.",
-  },
+  // {
+  //   name: "Portfolio",
+  //   image: "/images/portfolio.png",
+  //   weblink: "https://omarfarahportfolio.netlify.app/index.html",
+  //   github: "https://github.com/therealOfarah/Omar-s-Portfolio",
+  //   bio: "This app showcases all of my work and links to deployments for each project that I have done so far. It was built using HTML,CSS and Javascript.",
+  // },
+  // {
+  //   name: "Tic-Tac-Toe",
+  //   image: "/images/Tic-Tac-Toe.png",
+  //   weblink: "https://therealofarah-ttt.netlify.app",
+  //   github: "https://github.com/therealOfarah/Tic-Tac-Toe",
+  //   bio: "I created a web game that lets you play Tic-Tac-Toe. It was built using HTML,CSS and Javascript.",
+  // },
+  // {
+  //   name: "Ani-Review",
+  //   image: "/images/Screenshot.png",
+  //   weblink: "https://omarsanireview1.fly.dev/",
+  //   github: "https://github.com/therealOfarah/Ani-Review",
+  //   bio: "A web app that allows you to search up anime and helps you find what you are looking for. This app was built using MEN stack(MongoDb, Express ,Node.js), Anime API, and EJS.",
+  // },
   {
     name: "Fitness Bro",
     image: "/images/fitness-bro.png",
--- js/project.js
+++ js/project.js
@@ -7,80 +7,20 @@ const cardContainer = document.getElementById("card-container")
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
-function start(){
-  let zindex = 10;
-  
-  document.querySelector("div.card").click(function(e){
-    e.preventDefault();
 
-    let isShowing = false;
-
-    if (document.querySelector(this).classList.contains("show")) {
-      isShowing = true
-    }
-
-    if (document.querySelector("div.cards").classList.contains("showing")) {
-      // a card is already in view
-      document.querySelector("div.card.show")
-        .classList.remove("show");
-
-      if (isShowing) {
-        // this card was showing - reset the grid
-        document.querySelector("div.cards")
-          .classList.remove("showing");
-      } else {
-        // this card isn't showing - get in with it
-        document.querySelector(this)
-          .css({zIndex: zindex})
-          .classList.add("show");
-
-      }
-
-      zindex++;
-
-    } else {
-      // no cards in view
-      document.querySelector("div.cards")
-        .classList.add("showing");
-      document.querySelector(this)
-        .css({zIndex:zindex})
-        .classList.add("show");
-
-      zindex++;
-    }
-    
-  });
-};
-start()
 
 let projectData = projects.map(project =>
-  `<div class="cards">
-    <div class="card">
-    <div class="card__image-holder">
-      <img class="card__image" src="${project.image}" alt="wave" />
-    </div>
-    <div class="card-title">
-      <a href="#" class="toggle-info btn">
-        <span class="left"></span>
-        <span class="right"></span>
-      </a>
-      <h2>
-      ${project.name}
-      </h2>
-    </div>
-    <div class="card-flap flap1">
-      <div class="card-description">
-      ${project.bio}
-      </div>
-        <div class="card-flap flap2">
-          <div class="card-actions">
-          <a href="${project.weblink}" class="btn">Visit</a>
-          </div>
-        </div>
-      </div>
-    </div>
+  `
+  <div class="card" style="width: 18rem;">
+  <img src="${project.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${project.name}</h5>
+    <p class="card-text">${project.bio}</p>
+    <a href="${project.weblink}" class="btn btn-primary">Deploy</a>
+    <a href="${project.github}" class="btn btn-secondary">Github</a>
   </div>
-`
+</div>
+  `
 ).join("")
 
 cardContainer.innerHTML = projectData
